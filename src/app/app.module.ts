e4874c10872263b44d524babdd40be39b1aa5ae4
Frederic <fredericclaus@user601.alpaca.int>
2019-01-22 15:12:49 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductService } from './service/product.service';
+
+// define the routes
+const appRoutes: Routes = [
+  { path: 'add', component: ProductAddComponent },
+  //{ path: '**', component: AppComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, HttpClientModule,
+                  FormsModule, ReactiveFormsModule, RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, 
+                  ProductAddComponent],
+  providers:    [ ProductService ],
+  bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
